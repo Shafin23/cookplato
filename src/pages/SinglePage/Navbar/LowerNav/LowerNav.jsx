9694cfb5d2dfaeb08596bd9c32d951ef565148a0
shafin90 <mashrafiahnam1@gmail.com>
2024-03-13 13:59:23 -0700

    category button added in the navbar

--- src/pages/SinglePage/Navbar/LowerNav/LowerNav.jsx
+++ src/pages/SinglePage/Navbar/LowerNav/LowerNav.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { VscThreeBars } from "react-icons/vsc";
+import { IoIosArrowDown } from "react-icons/io";
 
 const LowerNav = () => {
     return (
-        <div>
-            
+        <div className=' w-full bg-[#fffaea]'>
+
+            <div className=' mx-auto w-[96vw] py-5'>
+
+                {/* Category button  ==============================*/}
+                <button className=' bg-[#fac250] flex justify-between items-center px-8 py-3 rounded'>
+                    <VscThreeBars />
+                    <span className=' mx-3 font-semibold'>CATEGORIES</span>
+                    <IoIosArrowDown />
+                </button>
+
+            </div>
+
         </div>
     );
 };
