9b51b7693bd0eafe8945af4393c34a99739357ab
shafin90 <mashrafiahnam1@gmail.com>
2024-03-21 01:37:29 -0700

    push

--- src/pages/Home/HeroSection/Board/Board.jsx
+++ src/pages/Home/HeroSection/Board/Board.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Board = () => {
     return (
         <div className=' mt-5 md:mt-0 w-full md:w-[28vw] h-auto md:h-[35vw] bg-[#fac250] flex flex-col justify-center items-center p-16'>
-            <h1 className=' mb-10 text-4xl font-bold text-center text-black'>Are you or do you want to be a cook?</h1>
+            <h1 className=' mb-10 text-2xl md:text-4xl font-bold text-center text-black'>Are you or do you want to be a cook?</h1>
 
             <article className=' text-center text-black text-lg w-full md:w-11/12 mb-4'>
                 Indiferent dacă ești student, bucătar amator sau profesionist, CookPlato te invită să împărtășești cele mai bune rețete cu toți!
--- src/pages/Home/HeroSection/Carouserl/Carousel.jsx
+++ src/pages/Home/HeroSection/Carouserl/Carousel.jsx
@@ -12,7 +12,7 @@ const Carousel = () => {
         },
         {
             image: "/assets/images/slider2.webp",
-            text: "Book your Cookplato chef and discover the authentic pleasure of a meal cooked personally for you"
+            text: "Bucură-te de experiențe culinare diverse și de calitate. Transformă un moment de sărbătoare într-un eveniment memorabil cu ajutorul bucătarilor CookPlato."
         }
     ];
 
@@ -29,14 +29,14 @@ const Carousel = () => {
 
 
     return (
-        <div className='w-full md:w-[64vw] h-[60vw] md:h-[35vw]'>
+        <div className='w-full md:w-[64vw] h-[120vw] md:h-[35vw]'>
             <div className="carousel w-full h-full relative overflow-hidden">
                 {slides.map((slide, index) => (
                     <div key={index} className={`carousel-item absolute w-full h-full transition-opacity duration-1000 ease-in-out ${index === currentSlide ? 'opacity-100' : 'opacity-0'}`}>
-                        <img src={slide.image} className="w-full brightness-50" alt={`Slide ${index + 1}`} />
-                        <div className='absolute w-full h-full flex flex-col justify-center items-center text-white' data-aos="fade-up" data-aos-duration="1000">
+                        <img src={slide.image} className=" w-[10500vw] absolute h-full brightness-50" alt={`Slide ${index + 1}`} />
+                        <div className='absolute w-full h-full flex flex-col justify-center items-center text-white ' data-aos="fade-up" data-aos-duration="1000">
                             <h1 className='text-center w-10/12 text-xl md:text-4xl font-bold mb-10'>{slide.text}</h1>
-                            <button className='bg-transparent px-5 py-2 border-white border-2 text-white font-semibold transition-all duration-500 hover:bg-white hover:text-black'>Find Cook</button>
+                            <button className='z-50 bg-transparent px-5 py-2 border-white border-2 text-white font-semibold transition-all duration-500 hover:bg-white hover:text-black'>Find Cook</button>
                         </div>
                     </div>
                 ))}
