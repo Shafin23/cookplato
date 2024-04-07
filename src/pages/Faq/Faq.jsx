10d22db4d15dd8cc6a52482d53e2b37b86b3b64c
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-04-07 12:24:03 +0600

    faq section responsive is done

--- src/pages/Faq/Faq.jsx
+++ src/pages/Faq/Faq.jsx
@@ -7,9 +7,10 @@ const Faq = () => {
 
   // TODO: RESPONSIVE OF FAQ PAGE IS NOT YET DONE
   return (
-    <div className="grid grid-cols-3 lg:justify-center w-4/5 lg:mx-auto gap-20 my-14">
+    // <div className="grid md:grid-cols-3 lg:justify-center border mx-4 w-11/12 lg:w-4/5 lg:mx-auto gap-20 my-14">
+    <div className="mx-4 my-10 grid grid-cols-1 lg:gap-7 md:grid-cols-2 lg:grid-cols-3">
       {/* contact page part */}
-      <div className="flex flex-col items-center text-start space-y-9">
+      <div className="flex flex-col mb-10 md:items-center text-start space-y-9">
         <div className="flex flex-col space-y-3">
           <h1 className="font-bold text-[#FAB528]">ORDERING INFORMATION</h1>
           <h3 className="font-semibold text-[#888888]">
@@ -24,7 +25,7 @@ const Faq = () => {
         </div>
       </div>
       {/* Faq 1st section */}
-        <div className="">
+        <div className="mb-5">
           <FaqData
             title={t("faq.whatIsCookplato")}
             color={'border-2 border-b border-[#EEEEEE]'}
@@ -46,7 +47,7 @@ const Faq = () => {
           />
         </div>
         {/* Faq 2nd Section */}
-        <div>
+        <div className="md:col-start-2 lg:col-start-auto">
           <FaqData
             title={t("faq.howCanIChooseAChef")}
             color={'border-2 border-b border-[#EEEEEE]'}
